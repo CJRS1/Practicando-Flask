63b129dd574887e697f64a51ce2f94c185fbc3c3
CJRS1 <reyes.christian@pucp.pe>
2023-05-03 12:15:49 -0700

    Se agregó codigo para agregar alumno

--- dia-1/app.py
+++ dia-1/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQLdb
+from flask_mysqldb import MySQL
+from os import environ #libreria propia de python
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 #almacena todas las variables de configuracion de la aplicacion de flask
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] =  'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'colegio'
-app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_HOST'] = environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] =  environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(environ.get('MYSQL_PORT'))
 #Sirve para conectarse a la base de datos en MySQL
-mysql = MySQLdb(app)
+mysql = MySQL(app)
 
 
 
@@ -25,13 +28,41 @@ def inicio():
 def pagina_inicial():
     return render_template('index.html')
 
-@app.route('/alumnos/',methods=['GET'])
+@app.route('/mostrar-alumnos/',methods=['GET'])
 def devolver_alumnos():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT*FROM alumnos")
     resultado = cursor.fetchall() #obtuiene todos los datos que estan en el execute
     print(resultado)
-    return {
-        'message':'Los alumnos son:'
-    }
+    resultado_final = []
+    for alumno in resultado:
+        alumno_diccionario={
+            'id':alumno[0],
+            'nombre':alumno[1],
+            'ape_paterno':alumno[2],
+            'ape_materno':alumno[3],
+            'correo':alumno[4],
+            'num_emergencia':alumno[5],
+            'curso_id':alumno[6],
+        }
+        resultado_final.append(alumno_diccionario)
+    # return {
+    #     'message':'Los alumnos son:',
+    #     'content': resultado_final
+    # }
+    return render_template('mostrar_alumnos.html',alumnos=resultado_final,mensaje='Hola desde Flask')
+
+@app.route('/agregar-alumno/',methods=['POST'])
+def agregar_alumnos():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT*FROM alumnos")
+    resultado = cursor.fetchall() #obtuiene todos los datos que estan en el execute
+
+    # return {
+    #     'message':'Los alumnos son:',
+    #     'content': resultado_final
+    # }
+    return render_template('agregar_alumno.html')
+
+
 app.run(debug=True)
